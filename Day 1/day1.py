df83d70ce82493ec17ff9c9c42147e7dec02ed1b
Elizabeth Forster <lizzieforster99@gmail.com>
2022-12-12 11:16:18 +0000

    Day 1 Complete

---
+++ Day 1/day1.py
@@ -0,0 +1,18 @@
+"""
+
+100 Days of Code : The Complete Python Pro Bootcamp by Dr. Angela Yu on Udemy
+
+Day 1 : Band Name Generator
+Date : 12/12/2022
+Author : E Forster
+
+"""
+
+print("Welcome to the Band Name Generator : \n")
+
+city = input("What is the name of the city you grew up in? \n")
+
+pet = input("Choose an animal : \n")
+
+print("A possible name for your band could be :", city, pet)
+
